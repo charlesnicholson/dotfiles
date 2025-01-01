e2849a27a288eb823ceda8e5d014db7cf85a26d7
Charles Nicholson <charles.nicholson@gmail.com>
2025-01-01 15:12:15 -0500

    fix gn

--- config/nvim/lua/plugins/gn.lua
+++ config/nvim/lua/plugins/gn.lua
@@ -1,15 +1,8 @@
 return {
   {
     "https://gn.googlesource.com/gn",
-    branch = "main",
-    lazy = false,
-    ft = "gn",
     config = function(plugin)
       vim.opt.rtp:append(plugin.dir .. "/misc/vim")
-      require("lazy.core.loader").packadd(plugin.dir .. "/misc/vim")
-    end,
-    init = function(plugin)
-      require("lazy.core.loader").ftdetect(plugin.dir .. "/misc/vim")
     end,
   }
 }
