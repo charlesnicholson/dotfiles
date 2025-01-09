b9826d1a8eff8ebe406c437acac514e12ec87770
Charles Nicholson <charles.nicholson@gmail.com>
2025-01-09 11:39:09 -0500

    quicker

--- config/nvim/lua/plugins/main.lua
+++ config/nvim/lua/plugins/main.lua
@@ -5,6 +5,7 @@ return {
   { "nvim-tree/nvim-web-devicons" },
   { "onsails/lspkind.nvim",       config = true },
   { "stevearc/dressing.nvim",     config = true },
+  { "stevearc/quicker.nvim",      config = true },
   { "chentoast/marks.nvim",       config = true },
   { "nvim-lualine/lualine.nvim",  config = true },
   { "lewis6991/gitsigns.nvim",    config = true },
