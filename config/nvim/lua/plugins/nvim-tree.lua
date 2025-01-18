7ee5b3640a1f7ab335c49ea199c588db21d34300
Charles Nicholson <charles.nicholson@gmail.com>
2025-01-18 15:33:35 -0500

    update, nvim-tree delete

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -4,7 +4,8 @@ return {
 
     config = function()
       require "nvim-tree".setup {
-        view = { width = 40, preserve_window_proportions = true }
+        view = { width = 40, preserve_window_proportions = true },
+        actions = { remove_file = { close_window = false } },
       }
       vim.keymap.set("n", "<leader>lf", ":NvimTreeFindFile<CR>", { silent = true })
     end
