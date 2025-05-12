510763b8d6840a9860fc85a75462b187b81b2433
Charles Nicholson <charles.nicholson@gmail.com>
2025-05-12 14:51:03 -0400

    ignore lazy-lock, re-add clangd toggle

--- config/nvim/lua/config/lsp/init.lua
+++ config/nvim/lua/config/lsp/init.lua
@@ -4,10 +4,11 @@ for _, server in ipairs(require "mason-lspconfig".get_installed_servers()) do
   require "lspconfig"[server].setup(opts)
 end
 
+vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>QF", vim.lsp.buf.code_action)
-vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, {})
+vim.keymap.set("n", "<leader>t", function() vim.cmd("ClangdSwitchSourceHeader") end)
 
 vim.lsp.set_log_level("off") -- "debug" or "trace"
 
