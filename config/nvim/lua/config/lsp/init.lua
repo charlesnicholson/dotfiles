1fdb6f05f257d3e698a4541a52b95fee634d6ab6
Charles Nicholson <charles.nicholson@gmail.com>
2025-07-11 13:41:02 -0400

    better lsp attachment

--- config/nvim/lua/config/lsp/init.lua
+++ config/nvim/lua/config/lsp/init.lua
@@ -4,12 +4,27 @@ for _, server in ipairs(require "mason-lspconfig".get_installed_servers()) do
   require "lspconfig"[server].setup(opts)
 end
 
-vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
-vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>QF", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>jd", vim.lsp.buf.definition, { silent = true })
-vim.keymap.set("n", "<leader>t", function() vim.cmd("ClangdSwitchSourceHeader") end)
+local function on_attach(client, bufnr)
+  vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "<leader>jd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>QF", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format { async = true } end, opts)
+
+  if client.name == "clangd" then
+    vim.keymap.set("n", "<leader>t", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
+  end
+end
+
+for _, server in ipairs(require "mason-lspconfig".get_installed_servers()) do
+  local ok, server_opts = pcall(require, "config.lsp.servers." .. server)
+  local opts = ok and server_opts or {}
+  opts.on_attach = on_attach
+  require "lspconfig"[server].setup(opts)
+end
 
 vim.lsp.set_log_level("off") -- "debug" or "trace"
 
